509263abc947615aec69605e32195360de606cf4
TernovAlv <121363092+TernovAlv@users.noreply.github.com>
2024-03-31 16:24:34 +0300

    Update Ubung_1.java
    I add string 6.

--- Ubung_1.java
+++ Ubung_1.java
@@ -3,5 +3,6 @@ import java.util.Arrays;
 public class Ubung_1 {
     public static void main(String[] args) {
         System.out.println("Ich learn  zu integraten.");
+        System.out.println("Ich learn Intell IDEA & Git zu  integraten.");
     }
 }
