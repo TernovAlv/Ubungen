2b0c0eaf179c0cf046465ae3653208dde001859d
TernovAlv <ternovaalv@gmail.com>
2024-03-31 16:40:21 +0300

    Update Ubung_1.java
    I add string 6.
    5. А я додала строку в код

--- Ubung_1.java
+++ Ubung_1.java
@@ -4,5 +4,6 @@ public class Ubung_1 {
     public static void main(String[] args) {
         System.out.println("Ich learn  zu integraten.");
         System.out.println("Ich learn Intell IDEA & Git zu  integraten.");
+        System.out.println("Es gelang mir _Вмене ВИЙШЛО!!!);
     }
 }
